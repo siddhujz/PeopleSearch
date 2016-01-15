ba185034aaed6633c1126fdce7a14ba9905573b1
Siddhartha Kodali <kodalisiddhu@gmail.com>
2016-01-15 13:56:51 -0600

    Introduced migrations.

---
+++ PeopleSearch/Model/ManagerDBContext.cs
@@ -0,0 +1,21 @@
+﻿using PeopleSearch.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleSearch.Model
+{
+    class ManagerDBContext : DbContext
+    {
+        public ManagerDBContext() : base("name=PeopleSearchDBConnectionString") 
+        {
+            Database.SetInitializer(new CreateDatabaseIfNotExists<ManagerDBContext>());
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<ManagerDBContext>());
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
