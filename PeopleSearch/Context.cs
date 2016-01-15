ac621a29cb647ac5522d7f4a1ae8b2ee0774fc65
Siddhartha Kodali <kodalisiddhu@gmail.com>
2016-01-15 12:59:09 -0600

    Introduced a context file, and a model class user with connection configuration in app.config.

---
+++ PeopleSearch/Context.cs
@@ -0,0 +1,19 @@
+﻿using PeopleSearch.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleSearch
+{
+    class Context: DbContext
+    {
+        public Context() : base("name=PeopleSearchDBConnectionString") 
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ PeopleSearch/Model/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleSearch.Model
+{
+    class User
+    {
+        public User()
+        {
+
+        }
+
+        public string UserId { get; set; }
+
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        public byte[] UserPhoto { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Interests { get; set; }
+
+        [MaxLength(50)]
+        public string AddressLine1 { get; set; }
+
+        [MaxLength(50)]
+        public string AddressLine2 { get; set; }
+
+        [MaxLength(50)]
+        public string City { get; set; }
+
+        [MaxLength(20)]
+        public string State { get; set; }
+
+        [MaxLength(20)]
+        public string Zipcode { get; set; }
+    }
+}
