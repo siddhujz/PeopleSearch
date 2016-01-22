419895c18ba849d2d1d1edaf6379706504865991
siddhujz <kodalisiddhu@gmail.com>
2016-01-22 12:54:48 -0600

    Introduced age converter function

---
+++ PeopleSearch/Services/DateOfBirthToAgeConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace PeopleSearch.Services
+{
+    class DateOfBirthToAgeConverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is DateTime)
+            {
+                DateTime dateOfBirth = (DateTime) value;
+
+                DateTime today = DateTime.Today;
+                int age = today.Year - dateOfBirth.Year;
+                if (dateOfBirth > today.AddYears(-age)) age--;
+
+                return age;
+            }
+
+            return null;
+        }
+
+        object IValueConverter.ConvertBack(object value,
+            Type targetType,
+            object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+    }
+}
